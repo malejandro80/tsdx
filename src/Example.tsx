5e9e0eb4db5dd6d4e02de1db2b4c77994cb6ee59
Miguel Bastidas <mbastidas@abatechnology.com>
2023-08-11 09:38:09 -0500

    test

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './tailwind.css';
-  import { StyledEngineProvider } from '@mui/material';
+  import './tailwind.css';
+    import { StyledEngineProvider } from '@mui/material';
 import Button from '@mui/material/Button';
  
 export const Test = () => {
   return (
     <StyledEngineProvider injectFirst>
-      <Button variant="contained" className="bg-slate-400 pt-8">
+      <Button variant="contained" className="bg-slate-950 pt-8 text-2xl">
         Hello World
       </Button>
     </StyledEngineProvider>
--- stories/3-Greeting.stories.js
+++ stories/3-Greeting.stories.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Example } from '../';
+import { Test } from '../';
 
 export default {
   title: 'Greeting',
 };
 
-export const jared = () => <Example />;
+export const jared = () => <Test />;
